d458298196c93d60728f5d61ad462e7bbfd5c76a
zxcskool <103977899+zxcskool@users.noreply.github.com>
2022-04-21 00:38:52 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -13,9 +13,7 @@ public class Question3
     Scanner intinput = new Scanner(System.in);
     int int1 = intinput.nextInt();
 
-    for (int i = int1; i < 1000000 && i > 1; i = i * i){
-      System.out.println(i);
-    }
+    System.out.println(int1 * int1);
     
   }
 }
