17ab88fc2b8499f538486879471feb8379c13b74
zxcskool <103977899+zxcskool@users.noreply.github.com>
2022-04-21 00:29:30 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -18,7 +18,7 @@ public class Question4
      * Hint: 2 loops are required. System.out.print(...) to print on single line.
      */
      
-    System.out.print("Enter an integer: ");
+    /*System.out.print("Enter an integer: ");*/
     Scanner userinput = new Scanner(System.in);
     int userint = userinput.nextInt();
 
